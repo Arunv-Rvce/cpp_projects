3a57d15d8f6d2fde2e396cdd3845ac055261dcae
Arun <arunvrvce@gmail.com>
2019-12-07 21:11:45 +0530

    initial commit to library management

---
+++ library_management/cmake-build-debug/CMakeFiles/library_management.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arun/CLionProjects/cpp_projects/library_management/Book.cpp" "/home/arun/CLionProjects/cpp_projects/library_management/cmake-build-debug/CMakeFiles/library_management.dir/Book.cpp.o"
+  "/home/arun/CLionProjects/cpp_projects/library_management/Customer.cpp" "/home/arun/CLionProjects/cpp_projects/library_management/cmake-build-debug/CMakeFiles/library_management.dir/Customer.cpp.o"
+  "/home/arun/CLionProjects/cpp_projects/library_management/Library.cpp" "/home/arun/CLionProjects/cpp_projects/library_management/cmake-build-debug/CMakeFiles/library_management.dir/Library.cpp.o"
+  "/home/arun/CLionProjects/cpp_projects/library_management/main.cpp" "/home/arun/CLionProjects/cpp_projects/library_management/cmake-build-debug/CMakeFiles/library_management.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ library_management/main.cpp
@@ -0,0 +1,16 @@
+#include <set>
+#include <map>
+#include <list>
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+#include "Book.h"
+#include "Customer.h"
+#include "Library.h"
+
+int main(void) {
+    Library();
+    return 1;
+}
