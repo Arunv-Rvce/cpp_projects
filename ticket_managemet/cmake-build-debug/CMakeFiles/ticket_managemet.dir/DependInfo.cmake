a69fbebfe7f517c7fc5ef227dd1a5e85ab428ecc
Arun <arunvrvce@gmail.com>
2019-12-08 23:00:29 +0530

    simple crud

---
+++ ticket_managemet/cmake-build-debug/CMakeFiles/ticket_managemet.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/arun/CLionProjects/cpp_projects/ticket_managemet/SqliteService.cpp" "/home/arun/CLionProjects/cpp_projects/ticket_managemet/cmake-build-debug/CMakeFiles/ticket_managemet.dir/SqliteService.cpp.o"
+  "/home/arun/CLionProjects/cpp_projects/ticket_managemet/Ticket.cpp" "/home/arun/CLionProjects/cpp_projects/ticket_managemet/cmake-build-debug/CMakeFiles/ticket_managemet.dir/Ticket.cpp.o"
+  "/home/arun/CLionProjects/cpp_projects/ticket_managemet/main.cpp" "/home/arun/CLionProjects/cpp_projects/ticket_managemet/cmake-build-debug/CMakeFiles/ticket_managemet.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ ticket_managemet/cmake-build-debug/CMakeFiles/ticket_managemet.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ticket_managemet.dir/SqliteService.cpp.o"
+  "CMakeFiles/ticket_managemet.dir/Ticket.cpp.o"
+  "CMakeFiles/ticket_managemet.dir/main.cpp.o"
+  "ticket_managemet"
+  "ticket_managemet.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ticket_managemet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
